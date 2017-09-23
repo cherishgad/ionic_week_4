d97e2f431ba9d770532a3dc2f3d6a7842e530aa9
youngjin <cherishgad@outlook.com>
2017-09-24 00:21:53 +0900

    the content structure update up to easy sliding

--- src/pages/tasklist/tasklist.ts
+++ src/pages/tasklist/tasklist.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ItemSliding } from 'ionic-angular';
 
 @Component({
   selector: 'page-tasklist',
   templateUrl: 'tasklist.html'
 })
 export class TaskListPage {
-
+  tasks: Array<any> = [];
   constructor(public navCtrl: NavController) {
-
+    this.tasks = [ 
+      {title:'Milk', status: 'open'}, 
+      {title:'Eggs', status: 'open'}, 
+      {title:'Syrup', status: 'open'}, 
+      {title:'Pancake Mix', status: 'open'} 
+    ];
+    
   }
-
+  addItem() {//message box for geting the user updating input 
+    let theNewTask: string = prompt("New Task"); 
+    if (theNewTask !== '') { 
+      this.tasks.push({ title: theNewTask, status: 'open' }); 
+    } 
+  }
+  markAsDone(slidingItem: ItemSliding, task: any) {
+     task.status = "done";
+     slidingItem.close(); 
+  } 
+  removeTask(slidingItem: ItemSliding, task: any) { 
+    task.status = "removed"; 
+    let index = this.tasks.indexOf(task); 
+    if (index > -1) { 
+      this.tasks.splice(index, 1); 
+    }
+    slidingItem.close(); 
+  }
 }
